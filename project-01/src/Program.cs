c3ab9a54232116e5e6b8a08f7377f81c865c0867
Kevin Frugier <kayl@kaylnet.com>
2021-01-31 22:27:22 +0100

    Rectangle implementation

--- project-01/src/Program.cs
+++ project-01/src/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace src
+namespace project01
 {
+    class Rectangle
+    {
+        public float side1;
+        public float side2;
+        public Rectangle(float sideA, float sideB)
+        {
+            side1 = sideA;
+            this.side2 = sideB;
+        }
+
+        public float calcArea()
+        {
+            return side1 * side2;
+        }
+
+        public float calcPerimeter()
+        {
+            return 2 * (side1 + side2);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rect1 = new Rectangle(4, 3);
+            Console.WriteLine(rect1.calcArea());
+            Console.WriteLine(rect1.calcPerimeter());
         }
     }
 }
