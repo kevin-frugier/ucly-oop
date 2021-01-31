ebdae2f62e0dd5cc8e59f0946cbc3230305d6cff
Kevin Frugier <kayl@kaylnet.com>
2021-01-31 22:54:24 +0100

    Added human example

---
+++ project-01/src/Human.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace project01 {
+    class Human {
+        public string firstName { get; }
+        public string lastName { get; }
+        public DateTime birth { get; }
+
+        public string name {
+            get {
+                return $"{firstName} {lastName}";
+            }
+        }
+
+        public int age {
+            get {
+                // https://stackoverflow.com/a/1404
+                DateTime today = DateTime.Today;
+                int age = today.Year - birth.Year;
+                if (birth.Date > today.AddYears(-age)) {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public Human(string firstName, string lastName, DateTime birth) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birth = birth;
+        }
+
+    }
+}
